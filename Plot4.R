128dfd54c7df4fff78fca2615b08c896ca642ccc
Vrinnda <60807316+Vrinnda@users.noreply.github.com>
2020-02-22 12:33:05 +0530

    Update Plot4.R

--- Plot4.R
+++ Plot4.R
@@ -1,27 +1,42 @@
 library("data.table")
-library("ggplot2")
 
-setwd("~/Desktop/datasciencecoursera/4_Exploratory_Data_Analysis/project2")
-path <- getwd()
-download.file(url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
-              , destfile = paste(path, "dataFiles.zip", sep = "/"))
-unzip(zipfile = "dataFiles.zip")
+setwd("~/Desktop/datasciencecoursera/4_Exploratory_Data_Analysis/project/data")
 
-# Load the NEI & SCC data frames.
-NEI <- data.table::as.data.table(x = readRDS("summarySCC_PM25.rds"))
-SCC <- data.table::as.data.table(x = readRDS("Source_Classification_Code.rds"))
+#Reads in data from file then subsets data for specified dates
+powerDT <- data.table::fread(input = "household_power_consumption.txt"
+                             , na.strings="?"
+)
 
-# Subset coal combustion related NEI data
-combustionRelated <- grepl("comb", SCC[, SCC.Level.One], ignore.case=TRUE)
-coalRelated <- grepl("coal", SCC[, SCC.Level.Four], ignore.case=TRUE) 
-combustionSCC <- SCC[combustionRelated & coalRelated, SCC]
-combustionNEI <- NEI[NEI[,SCC] %in% combustionSCC]
+# Prevents Scientific Notation
+powerDT[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
 
-png("plot4.png")
+# Making a POSIXct date capable of being filtered and graphed by time of day
+powerDT[, dateTime := as.POSIXct(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S")]
 
-ggplot(combustionNEI,aes(x = factor(year),y = Emissions/10^5)) +
-  geom_bar(stat="identity", fill ="#FF9999", width=0.75) +
-  labs(x="year", y=expression("Total PM"[2.5]*" Emission (10^5 Tons)")) + 
-  labs(title=expression("PM"[2.5]*" Coal Combustion Source Emissions Across US from 1999-2008"))
+# Filter Dates for 2007-02-01 and 2007-02-02
+powerDT <- powerDT[(dateTime >= "2007-02-01") & (dateTime < "2007-02-03")]
+
+png("plot4.png", width=480, height=480)
+
+par(mfrow=c(2,2))
+
+# Plot 1
+plot(powerDT[, dateTime], powerDT[, Global_active_power], type="l", xlab="", ylab="Global Active Power")
+
+# Plot 2
+plot(powerDT[, dateTime],powerDT[, Voltage], type="l", xlab="datetime", ylab="Voltage")
+
+# Plot 3
+plot(powerDT[, dateTime], powerDT[, Sub_metering_1], type="l", xlab="", ylab="Energy sub metering")
+lines(powerDT[, dateTime], powerDT[, Sub_metering_2], col="red")
+lines(powerDT[, dateTime], powerDT[, Sub_metering_3],col="blue")
+legend("topright", col=c("black","red","blue")
+       , c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  ")
+       , lty=c(1,1)
+       , bty="n"
+       , cex=.5) 
+
+# Plot 4
+plot(powerDT[, dateTime], powerDT[,Global_reactive_power], type="l", xlab="datetime", ylab="Global_reactive_power")
 
 dev.off()
